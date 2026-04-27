52ce2fd5c2feac42cb5c37bb2ab4a8feaf54789d
Nguyen Dung Hiep <121426958+hionex@users.noreply.github.com>
2026-04-27 16:04:26 +0700

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.java
@@ -0,0 +1,23 @@
+// Bubble Sort
+// Each iteration, max value in the unsorted part
+// will be moved to the end of unsorted part.
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            boolean swapped = false; 
+            for(int j = 0; j < nums.length - i - 1; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(!swapped) break; 
+        }
+
+        return nums;
+    }
+}
